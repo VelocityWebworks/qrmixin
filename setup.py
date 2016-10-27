a0c180d69203a6aa2a6318f5f9bb15fc1306518a
Preston Wagner <preston@velocitywebworks.com>
2016-10-27 20:17:42 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-qrmixin',
-    version='1.0.1',
+    version='1.1.0',
     description="Provides a QR code generating mixin for django models",
     author="Danemco, LLC",
     author_email='dev@velocitywebworks.com',
-    url='https://bitbucket.org/freakypie/django-qrmixin',
+    url='https://github.com/VelocityWebworks/qrmixin/commits/master',
     packages=find_packages(),
     install_requires=['qrcode'],
 )
